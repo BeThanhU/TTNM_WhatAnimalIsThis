894a12a6f10ef5d203a024f547435d3b26422ed6
TrixiePh <98095658+TrixiePh@users.noreply.github.com>
2022-01-24 21:18:44 +0700

    nhien upload FE

---
+++ TTNM_FE/js/details.js
@@ -0,0 +1,44 @@
+const productImages = document.querySelectorAll(".product-images img");
+const productImageSlide = document.querySelector(".image-slider");
+
+let activeImageSlide = 0;
+
+productImages.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        productImages[activeImageSlide].classList.remove('active');
+        item.classList.add('active');
+        productImageSlide.style.backgroundImage = `url('${item.src}')`;
+        activeImageSlide = i;
+    })
+})
+
+
+//color buttons
+
+const colorBtns = document.querySelectorAll('.color-radio-btn');
+let checkedBtn = 0;
+
+colorBtns.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        colorBtns[checkedBtn].classList.remove('check');
+        item.classList.add('check');
+        checkedBtn = i;
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ``
